52fa27effc874f4189c8c43c8f30d361bf038fa9
adeonita <adeonita.sousa@gmail.com>
2023-05-29 19:44:04 -0300

    feat: separate writing to file by agent type

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -9,15 +9,26 @@ This is a test class to assess the executables of this project.
 """
 class BinTest(unittest.TestCase):
 
+    def writeInSpecificFile(self, path, agent, score, totalCost, miliSecond):
+        with open(path, 'a') as f:
+            f.write(agent + ',')
+            f.write(score + ',')
+            f.write(str(totalCost) + ',')
+            f.write(miliSecond + '\n')
+
     def test_pacman(self):
         agents = ["GreedyAgent","DepthSearchAgent", "BreadthSearchAgent"]
 
-        absolutePath = os.path.dirname(os.path.abspath(__file__)) 
-        relativePath = 'results/result.csv'
+        absolutePath = os.path.dirname(os.path.abspath(__file__))
+        basePath = absolutePath.replace('tests', 'results/')
 
-        path = absolutePath.replace('tests', relativePath)
-       
-        with open (path, 'w') as f:
+        resultPath = basePath + 'result.csv'
+        breadthSearchPath = basePath + 'breadthSearchAgent/bsa.csv'
+        depthSearchPath = basePath + 'depthSearchAgent/dsa.csv'
+        greedyPath = basePath + 'greedyAgent/ga.csv'
+
+
+        with open (resultPath, 'w') as f:
             f.write('Agent,Scores,Cost,Milis\n')
         for agent in agents:
             map = 1
@@ -31,12 +42,17 @@ class BinTest(unittest.TestCase):
                         totalCost += a.totalCost
                 scores = [game.state.getScore() for game in games]
                 score = str(sum(scores))
-                milis = str((ended - started) * 1000)
-                with open(path, 'a') as f:
-                    f.write(agent + ',')
-                    f.write(score + ',')
-                    f.write(str(totalCost) + ',')
-                    f.write(milis + '\n')
+                miliSecond = str((ended - started) * 1000)
+                totalCost = str(totalCost)
+
+                self.writeInSpecificFile(resultPath, agent, score, totalCost, miliSecond)
+
+                if agent == 'BreadthSearchAgent':
+                    self.writeInSpecificFile(breadthSearchPath, agent, score, totalCost, miliSecond)
+                elif agent == 'DepthSearchAgent':
+                    self.writeInSpecificFile(depthSearchPath, agent, score, totalCost, miliSecond)
+                else:
+                    self.writeInSpecificFile(greedyPath, agent, score, totalCost, miliSecond)
                 map += 1
 
 
